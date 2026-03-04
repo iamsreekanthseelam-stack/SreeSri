05e25b6d80ea352fdbf25bfa86e9d378da26609e
SreeSri4 <101240173+SreeSri4@users.noreply.github.com>
2026-03-04 09:14:40 +0530

    Implement Google Drive service functions

---
+++ src/frontend/googleDriveService.ts
@@ -0,0 +1,69 @@
+// src/googleDriveService.ts
+
+const CLIENT_ID = "993142680699-qnm9j6ru6olsvsqo64ckbmos8rfhrssn.apps.googleusercontent.com";
+const SCOPES = "https://www.googleapis.com/auth/drive.file";
+
+let tokenClient: any;
+
+export function initGoogleAuth() {
+  return new Promise<void>((resolve) => {
+    window.gapi.load("client", async () => {
+      await window.gapi.client.init({
+        discoveryDocs: [
+          "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest",
+        ],
+      });
+
+      tokenClient = window.google.accounts.oauth2.initTokenClient({
+        client_id: CLIENT_ID,
+        scope: SCOPES,
+        callback: "",
+      });
+
+      resolve();
+    });
+  });
+}
+
+export function login() {
+  return new Promise<void>((resolve, reject) => {
+    tokenClient.callback = (resp: any) => {
+      if (resp.error) reject(resp);
+      else resolve();
+    };
+
+    tokenClient.requestAccessToken({ prompt: "consent" });
+  });
+}
+
+export async function readJsonFile(fileId: string) {
+  const response = await window.gapi.client.drive.files.get({
+    fileId: fileId,
+    alt: "media",
+  });
+
+  return JSON.parse(response.body);
+}
+
+export async function updateJsonFile(fileId: string, data: any) {
+  const boundary = "foo_bar_baz";
+
+  const multipartRequestBody =
+    `--${boundary}\r\n` +
+    "Content-Type: application/json\r\n\r\n" +
+    JSON.stringify({ mimeType: "application/json" }) +
+    `\r\n--${boundary}\r\n` +
+    "Content-Type: application/json\r\n\r\n" +
+    JSON.stringify(data) +
+    `\r\n--${boundary}--`;
+
+  await window.gapi.client.request({
+    path: `/upload/drive/v3/files/${fileId}`,
+    method: "PATCH",
+    params: { uploadType: "multipart" },
+    headers: {
+      "Content-Type": `multipart/related; boundary=${boundary}`,
+    },
+    body: multipartRequestBody,
+  });
+}
