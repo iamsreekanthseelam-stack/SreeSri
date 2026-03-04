d05795a25eba63178b1cdb1ef6c9d990156d6c46
SreeSri4 <101240173+SreeSri4@users.noreply.github.com>
2026-03-04 13:43:49 +0530

    Update main.tsx

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -1,7 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { InternetIdentityProvider } from "./hooks/useInternetIdentity";
 import "../index.css";
 
 BigInt.prototype.toJSON = function () {
